edfab145dc3fea769c8022d99463811e64714cf5
Coco-02734 <122201902734@mhs.dinus.ac.id>
2021-03-30 23:41:54 +0700

    Upload Awal

---
+++ application/Modules/Seller/controllers/Seller.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Seller extends CI_Controller
+{
+
+    public function index()
+    {
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $this->load->view('template/header', $data);
+        $this->load->view('template/sidebar', $data);
+        $this->load->view('template/topbar', $data);
+        $this->load->view('index', $data);
+        $this->load->view('template/footer');
+    }
+}
